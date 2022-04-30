45908afbaf4456c1800230b6d3e9c1f695bf7b04
Kirill Demidov <kademidov@gmail.com>
2022-04-30 20:28:33 +0300

    Разметка и стилизация десктопа для внутренних страниц

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,35 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  let isMenuOpen = false;
+
+  $('.j-burger').on( 'click' , function() {
+
+    $('.j-menu').slideToggle();
+    // if (isMenuOpen) {
+    //   $('.j-menu').slideUp();
+    //   isMenuOpen = false;
+    // } else {
+    //   $('.j-menu').slideDown();
+    //   isMenuOpen = true;
+    // }
+  });
+
+
+  // Аккордионы
+  let prevBtn;
+
+  $('.j-accordion-btn').on('click', function(){
+
+    if (prevBtn === this) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.j-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = this;
+
+  });
+
+});
+
